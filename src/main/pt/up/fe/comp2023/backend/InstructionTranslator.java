d69f5e1a4cf4201d36fab335ae7e56700dfce181
iamaccosta <up201905916@up.pt>
2023-05-21 00:07:15 +0100

    while and if attemp2

--- src/main/pt/up/fe/comp2023/backend/InstructionTranslator.java
+++ src/main/pt/up/fe/comp2023/backend/InstructionTranslator.java
@@ -331,10 +331,10 @@ public class InstructionTranslator {
                 jasminInstruction.append("iadd");
             }
             case SUB -> {
-                if (!leftElement.isLiteral() && rightElement.isLiteral()) {
+                /*if (!leftElement.isLiteral() && rightElement.isLiteral()) {
                     manageStack(1);
                     return iinc((LiteralElement) rightElement, (Operand) leftElement, method, operationType);
-                }
+                }*/
 
                 jasminInstruction.append(getCorrespondingLoad(leftElement, method)).append("\n");
                 jasminInstruction.append(getCorrespondingLoad(rightElement, method)).append("\n");
