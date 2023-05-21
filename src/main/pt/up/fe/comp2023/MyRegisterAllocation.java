9a04abead5be73edf8e2a3e91bd874e365e59e63
Andre Soares <andre.soarestc13@gmail.com>
2023-05-21 21:56:49 +0100

    another test

--- src/main/pt/up/fe/comp2023/MyRegisterAllocation.java
+++ src/main/pt/up/fe/comp2023/MyRegisterAllocation.java
@@ -1,7 +1,12 @@
 package pt.up.fe.comp2023;
 
+import org.specs.comp.ollir.Descriptor;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp2023.registerAlloc.MyLifeTime;
+import pt.up.fe.comp2023.registerAlloc.MyLifeTimeCalculator;
+import pt.up.fe.comp2023.registerAlloc.MyNode;
+
+import java.util.HashMap;
 
 
 public class MyRegisterAllocation {
@@ -9,14 +14,33 @@ public class MyRegisterAllocation {
         System.out.println(ollirResult.getConfig());
         OllirResult optimized = ollirResult;
         if (ollirResult.getConfig().get("registerAllocation") != null) {
-
-            MyLifeTime lifeTime = new MyLifeTime(ollirResult);
-            lifeTime.allocate();
-
+            MyRegisterAllocation allocator = new MyRegisterAllocation();
+            allocator.allocate(ollirResult);
         }
-
-
-
+        
         return optimized;
+    }
+
+
+    public void allocate(OllirResult ollirResult) {
+        ollirResult.getOllirClass().buildCFGs();
+        Integer registerNum = Integer.parseInt(ollirResult.getConfig().get("registerAllocation"));
+        for (int i=0;i<ollirResult.getOllirClass().getMethods().size();i++){
+            MyLifeTimeCalculator methodLifetime = new MyLifeTimeCalculator(ollirResult.getOllirClass().getMethods().get(i),ollirResult);
+            methodLifetime.calcInOut();
+            methodLifetime.buildInterferenceGraph();
+            methodLifetime.colorInterferenceGraph(registerNum);
+            HashMap<String, Descriptor> varTable = methodLifetime.getMethod().getVarTable();
+            for (MyNode node: methodLifetime.getInterferenceGraph().localVars) {
+                varTable.get(node.name).setVirtualReg(node.getReg());
+            }
+            for (MyNode node: methodLifetime.getInterferenceGraph().params) {
+                varTable.get(node.name).setVirtualReg(node.getReg());
+            }
+
+            if (varTable.get("this") != null) {
+                varTable.get("this").setVirtualReg(0);
+            }
+        }
     }
 }
